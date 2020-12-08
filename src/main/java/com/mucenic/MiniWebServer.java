50f5e35bc4b6d292936c36a93ed63ad8933aa480
Mucenicalexandru <mucenic.alexandru@gmail.com>
2020-12-08 12:11:48 +0200

    added @WebRoute interface annotation

--- src/main/java/com/mucenic/MiniWebServer.java
+++ src/main/java/com/mucenic/MiniWebServer.java
@@ -7,7 +7,6 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import java.util.Arrays;
 
 public class MiniWebServer {
 
