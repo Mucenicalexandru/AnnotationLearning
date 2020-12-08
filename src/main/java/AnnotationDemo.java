e6fe46c18d3edf9b843db5543aed00768a3bf201
Mucenicalexandru <mucenic.alexandru@gmail.com>
2020-12-08 10:52:03 +0200

    project questions added

--- src/main/java/AnnotationDemo.java
+++ src/main/java/AnnotationDemo.java
@@ -5,6 +5,20 @@ import lombok.Setter;
 import java.lang.annotation.*;
 import java.util.ArrayList;
 
+// Where can you put annotations?
+// ====> Class annotations, interface annotation, methods annotations
+
+// What does @Target and @Retention mean?
+// ====> When we declare an @interface annotation, then we need to specify the Target and Retention. Target indicates the contexts in which an annotation type is applicable (FIELD, METHOD, etc)
+// Retention ===> Indicates how long annotations with the annotated type are to be retained.  If no Retention annotation is present on an annotation type declaration, the retention policy defaults to RetentionPolicy.CLASS.
+
+//Name 3 annotations that you have seen!
+// ====> @Override, @BeforeEach, @AfterEach, @Getter, @Setter, @Retention, @Target, @WebServlet, @Controller
+
+//Why do annotations exist?
+// Annotations provide data about a program that is not part of the program itself. They have no direct effect on the operation of the code they annotate.
+
+
 @FunctionalInterface
 //you can have only one method in the interface
 interface Abc{
@@ -37,7 +51,6 @@ class B extends A{
 //Retention -> Until when to use this annotation - in our case until runtime
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-
 @interface SmartPhone{
     String os() default "Symbian";
     int version() default 1;
@@ -65,7 +78,7 @@ public class AnnotationDemo {
 
         //we use Reflection API to fetch the values of the annotation
         //With Reflection API we can get the features of the object
-        Class classObject = obj.getClass();
+        Class classObject = phone.getClass();
         Annotation annotation = classObject.getAnnotation(SmartPhone.class);
         SmartPhone smartPhone = (SmartPhone) annotation;
 
